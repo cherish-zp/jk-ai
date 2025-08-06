7beff8411cf14500bc0b170b466442c4bf34bc2c
zhangpeng <zhangpeng.yf@act-telecom.com>
2025-08-06 11:00:45 +0800

    llm 联调 chromadb

---
+++ rag_law/Config.py
@@ -0,0 +1,10 @@
+class Config:
+    EMBED_MODEL_PATH = r"/Users/zhangpeng/code_bigmodel/jk-ai/00-model/modelscope/embedding_model/sungw111/text2vec-base-chinese-sentence"
+    LLM_MODEL_PATH = r"/Users/zhangpeng/code_bigmodel/jk-ai/00-model/modelscope/Qwen/Qwen1.5-1.8B-Chat"
+
+    DATA_DIR = "/Users/zhangpeng/code_bigmodel/jk-ai/rag_law/data"
+    VECTOR_DB_DIR = "/Users/zhangpeng/code_bigmodel/jk-ai/rag_law/chroma_db"
+    PERSIST_DIR = "/Users/zhangpeng/code_bigmodel/jk-ai/rag_law/storage"
+
+    COLLECTION_NAME = "chinese_labor_laws"
+    TOP_K = 3
