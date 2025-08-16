c71134cdf892b2930faaf33ce42b190c0e6fbb72
zhangpeng <zhangpeng.yf@act-telecom.com>
2025-08-16 21:17:52 +0800

    特征的线性变换理解

--- pytorch/P21_nn_linear.py
+++ pytorch/P21_nn_linear.py
@@ -13,7 +13,8 @@ class TuDou(nn.Module):
     def __init__(self):
         super(TuDou, self).__init__()
         # torch.Size([64, 3, 32, 32])  3072 是单个样本的长度 3*32*32  , 【理解是 3072 的分类任务，分解成了10的分类任务】
-        self.linear1 = Linear(3*32*32, 10)
+        # 其实就是网络计算 output = input @ weight.T + bias   output 10 是结果， input 是输入 3072 ， weight 和bias 是可训练参数，在变换，讲 3072高纬度映射到 10 上
+        self.linear1 = Linear(3*32*32, 10)   # 定义全连接层
     def forward(self, input):
         output = self.linear1(input)
         return output
